e272b5d182b5785292db5de0b3dfe7716875403a
ErickCu <erick.cuenca.cl@gmail.com>
2023-11-27 00:56:47 -0500

    nuevo

---
+++ Quiz/urls.py
@@ -0,0 +1,38 @@
+from django.urls import path
+
+from .views import (
+    inicio,
+    jugar,
+    sinTiempo,
+    tablero,
+    sinTiempo,
+    resultado,
+    comentario,
+    lista_carreras,
+    lista_cuestionarios, lista_materias, lista_preguntas,
+    salir,register,mostrar_preguntas,mostrar)
+
+urlpatterns = [
+
+    path('', inicio, name='inicio'),
+    path('tablero/', tablero, name='tablero'),
+    # path('jugar/', jugar, name='jugar'),
+    path('jugar/<int:id_cuestionario>/', jugar, name='jugar'),
+    path('sinTiempo/', sinTiempo, name='sinTiempo'),
+    path('resultado/<int:pregunta_respondida_pk>', resultado, name='resultado'),
+    path('comentario/', comentario, name='comentario'),
+    path('carreras/', lista_carreras, name='lista_carreras'),
+    path('materias/<int:id_carrera>/', lista_materias, name='lista_materias'),
+    path('cuestionarios/<int:id_materia>/',
+         lista_cuestionarios, name='lista_cuestionarios'),
+    path('preguntas/<int:id_cuestionario>/',
+         lista_preguntas, name='lista_preguntas'),
+    path('logout/', salir, name='exit'),
+    path('register/', register, name='register'),
+    path('prueba/',mostrar_preguntas,name='prueba'),
+    path('mostrar/<int:id_cuestionario>/',mostrar, name='mostrar'),
+    path('mostrar/<int:id_cuestionario>/',mostrar, name='mostrar'),
+
+
+]
+
